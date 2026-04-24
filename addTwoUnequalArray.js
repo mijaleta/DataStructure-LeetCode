fad988552029a7d05977ef097b2c82f53fd27215
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-04-24 14:15:44 +0300

    updated

---
+++ addTwoUnequalArray.js
@@ -0,0 +1,16 @@
+function addUnequalArrays(arra1, array2) {
+  let result = [];
+  let maxLength = Math.max(arra1.length, array2.length);
+
+  for (i = 0; i < maxLength; i++) {
+    let x = arra1[i] || 0;
+    let y = array2[i] || 0;
+    result.push(x + y);
+  }
+
+  return result;
+}
+
+let finalResult = addUnequalArrays([1, 2, 3], [4, 5]);
+
+console.log("The final result is the followng " + finalResult);
