75db6bfeffabb8d17b1a01b13c3e879505338460
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-04-27 11:45:14 +0300

    updated

--- Two Sum.js
+++ Two Sum.js
@@ -13,7 +13,7 @@ var twoSum = function (nums, target) {
     seen.set(nums[i], i);
   }
 };
-
+// the folloing are the output
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
