ccc707f6a67a3950889e61f9a7ed42390d84786e
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-05-04 13:29:49 +0300

    updated

--- addTwoUnequalArray.js
+++ addTwoUnequalArray.js
@@ -9,10 +9,9 @@ function addUnequalArrays(arra1, array2) {
     let y = array2[i] || 0;
     result.push(x + y);
   }
-
+// here the result is 
   return result;
 }
 
 let finalResult = addUnequalArrays([1, 2, 3], [4, 5]);
-
 console.log("The final result is the followng " + finalResult);
