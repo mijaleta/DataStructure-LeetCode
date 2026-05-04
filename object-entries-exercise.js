006a05eb4fc12c5f4188123a599e719e8e4d2589
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-05-04 13:39:15 +0300

    updated

---
+++ object-entries-exercise.js
@@ -0,0 +1,6 @@
+const user = {name:"Miretu", age:25}
+
+// converting to entries or array pairs 
+ const formattedUser = Object.entries(user)
+
+ console.log("This one is after the object is chnaged to array for iterations purpose " ,formattedUser );
