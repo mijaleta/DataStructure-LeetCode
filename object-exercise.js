2101b40b5ea275583b94c44a3a32d87f9807ea03
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-05-06 14:50:31 +0300

    uodated

--- object-exercise.js
+++ object-exercise.js
@@ -1,3 +1,4 @@
 // Object - stores key and value pairs
 const obj = { name: "John", age: 25, city: "NYC" };
 console.log(obj); // { name: "John", age: 25, city: "NYC" }
+console.log(obj); // { name: "John", age: 25, city: "NYC" }
