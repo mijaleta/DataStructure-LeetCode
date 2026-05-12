514a4a7f38d9e6a235ab68deaec80e51cd86f80e
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-05-12 16:58:40 +0300

    updated

---
+++ Counter copy.js
@@ -0,0 +1,17 @@
+function createCounter(initialValue) {
+  let count = initialValue;
+  return function () {
+    return count++;
+  };
+}
+const counter = createCounter(1);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
