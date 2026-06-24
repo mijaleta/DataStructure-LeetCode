f54e335fa206ddaa82107b477e898c537b20996e
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-24 10:44:39 +0300

    done

---
+++ array-reduce-transformation.js
@@ -0,0 +1,22 @@
+// # Intuition
+// <!-- Describe your first thoughts on how to solve this problem. -->
+
+// # Approach
+// <!-- Describe your approach to solving the problem. -->
+
+// # Complexity
+// - Time complexity:
+// <!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+// - Space complexity:
+// <!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+// # Code
+
+var reduce = function(nums, fn, init) {
+    let val = init;
+    for (let num of nums) {
+        val = fn(val, num);
+    }
+    return val;
+};
