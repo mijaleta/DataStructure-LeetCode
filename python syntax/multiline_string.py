5428904944b9ffae977290abfd09a8e19c680250
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-25 14:28:08 +0300

    multiline stirng usng the three double or single quoate is done

---
+++ python syntax/multiline_string.py
@@ -0,0 +1,14 @@
+biblography = """ My name is miretu Jaleta
+i am a full stack developer and machine learnign engineer 
+and i am a graduate of the jimma univeristy 
+  """
+print(biblography)
+
+biblography2 ='''my name is miretu jaleta i graduated from the jimma univeristy 
+
+this one is the second history of mine and it is that. 
+i have four sibling
+
+'''
+
+print(biblography2)
