d3a631151dbe5f38c8704a69cdf0c7035f02bcac
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-25 16:14:00 +0300

    done

---
+++ python syntax/upper_and_lower.py
@@ -0,0 +1,12 @@
+x = " Hello world "
+# original string
+print("original string ", x)
+# change all to capital
+print("string to uppercase", x.upper())
+# chnage all to small 
+print("string to lower case ", x.lower())
+# for removing the space between the string use the followng
+print("space removal", x.strip())
+#  if i want to replace the the above hellow world with the hello affrica i can do the followng 
+
+print("replace string", x.replace("world", "africa"))
