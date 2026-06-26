6897945eda5799fa63c7ce65e96f2add038d71da
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-26 09:23:33 +0300

    f- string isndie the python is added

---
+++ python syntax/f_string.py
@@ -0,0 +1,10 @@
+# the python f string is like the one backtic insdie the js 
+
+x= "miret"
+
+full_info = f"my name is {x}"
+
+print(full_info)
+
+
+# simply the f stirngis the template literal inside  the  like the one isndie the js 
