d75084e0c03cc19095203f2d6d2e86f627fcdb76
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-26 11:54:06 +0300

    range which is built is added to the abvoe

---
+++ python syntax/range.py
@@ -0,0 +1,8 @@
+# we use the range for the following reason to list the list for example range(3) means [0,1,2]
+
+
+
+names = ["miretu", "taku", "chala"]
+
+for i in range(len(names)):
+    print(names[i])
