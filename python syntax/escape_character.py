767b79f9c91668fcdeab4129edc27b6a7a850bb1
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-26 09:30:39 +0300

    done

---
+++ python syntax/escape_character.py
@@ -0,0 +1,10 @@
+# the following does not matter
+
+print("my name is 'miretu'")
+#  but the folllowing does has a problem 
+
+# print("my name is "miretu" ")
+
+# but the following does not matter
+
+print("my name is \"miretu\" ")
