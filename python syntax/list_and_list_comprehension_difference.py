c5a21fb7750ebf28148ea139957d0c12c6c2d0c1
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-29 15:01:10 +0300

    this one is also the list compherension which is very important tisndie the django rest framwork when i filter or use soedata iandie the backend

--- python syntax/list_and_list_comprehension_difference.py
+++ python syntax/list_and_list_comprehension_difference.py
@@ -17,3 +17,25 @@ fruits = ["banana", "apple", "mango"]
 
 capitalized_fruits = [fruit.upper() for fruit in fruits]
 print(capitalized_fruits)
+
+
+# for the given name if it is kortu make it obboletti
+
+
+names_of_family = ["miretu", "kisi","taku","kortu"]
+
+pure_family = [name if name !="kortu" else  "kisi" for name in names_of_family]
+
+print(pure_family)
+
+
+
+# list of only the number not the letter
+
+mix_of_number_and_letter = ['m', '1', 'f']
+
+# only print th eletter 
+
+only_letter = [name if name != '1' else "not letter" for name in mix_of_number_and_letter]
+
+print(only_letter)
