ced10593eb19888a7fd901d81aac08828f754ed4
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-29 14:32:43 +0300

    done

---
+++ python syntax/list_and_list_comprehension_difference.py
@@ -0,0 +1,9 @@
+names = ["miretu", "taku","eebise"]
+
+for name in names:
+    print(name)
+
+# when we use the the list comprehension we can do like the following 
+
+name_list = [name for name in names]
+print(name_list) 
