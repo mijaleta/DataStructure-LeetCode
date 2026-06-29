8a15be30df9367d49276b6eae169c31e83ba6796
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-29 14:36:38 +0300

    capitalized fruits

--- python syntax/list_and_list_comprehension_difference.py
+++ python syntax/list_and_list_comprehension_difference.py
@@ -7,3 +7,13 @@ for name in names:
 
 name_list = [name for name in names]
 print(name_list) 
+
+
+
+# making cpiatle the each letter of the word
+
+
+fruits = ["banana", "apple", "mango"]
+
+capitalized_fruits = [fruit.upper() for fruit in fruits]
+print(capitalized_fruits)
