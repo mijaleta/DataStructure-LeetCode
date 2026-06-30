355c73697d40a52b27d91335b493a8d24891aebd
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-06-30 08:47:26 +0300

    sorting of the array of string and number

---
+++ python syntax/sorting.py
@@ -0,0 +1,14 @@
+# the following is all about the sorting
+
+names = ["eebbisee","miretu","jaleta"]
+
+sortedName = names.sort()
+print("the sorted names are ", sortedName)
+
+# the following is sorted number 
+
+numbers = [2,5,1,8,3]
+
+sortedNumber = sorted(numbers)
+
+print("the sorted number are the following", sortedNumber)
