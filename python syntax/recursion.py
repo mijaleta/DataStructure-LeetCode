6e549c3f33f2fb55f2cd9525f0a068a80ef8f32b
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-07-01 09:13:17 +0300

    done the recursion

--- python syntax/recursion.py
+++ python syntax/recursion.py
@@ -10,3 +10,17 @@ def factorial(n):
     return n * factorial(n-1)
 
 print(factorial(3))
+
+
+# the folowing is another recursoion function which is all abotu the going down from the top 5 stair to the bottom 
+
+def countDown(n):
+    if n == 0:
+        return 0
+    print(n)
+    countDown(n-1)
+
+    
+countDown(3)
+
+ 
